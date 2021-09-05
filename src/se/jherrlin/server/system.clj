2b5fdae64efed22ca634bccc5586cecb99404c6a
John Herrlin <john.herrlin@fortnox.se>
2021-09-05 21:44:01 +0200

    system starts to work

--- src/se/jherrlin/server/system.clj
+++ src/se/jherrlin/server/system.clj
@@ -18,7 +18,42 @@
   (:gen-class))
 
 
+(defonce game-state
+  (atom {:players    {1 {:position    [1 1]
+                         :fire-length 3
+                         :id          1
+                         :sign        "1"}}
+         :stones     [[4 1] [3 3]]
+         :board      (board/init 6)
+         :dead-users {}
+         :bombs      [#_{:user-id              1
+                         :bomb-position-xy     [3 2]
+                         :fire-length          3
+                         :bomb-added-timestamp (java.util.Date.)}]
+         :fire       [#_{:user-id              6
+                         :fire-position-xy     [3 2]
+                         :fire-start-timestamp (java.util.Date.)}]}))
 
+(comment
+  (reset! game-state
+          {:players    {1 {:position    [1 1]
+                         :fire-length 3
+                         :id          1
+                         :sign        "1"}}
+         :stones     [[4 1] [3 3]]
+         :board      (board/init 6)
+         :dead-users {}
+         :bombs      [#_{:user-id              1
+                         :bomb-position-xy     [3 2]
+                         :fire-length          3
+                         :bomb-added-timestamp (java.util.Date.)}]
+         :fire       [#_{:user-id              6
+                         :fire-position-xy     [3 2]
+                         :fire-start-timestamp (java.util.Date.)}]})
+  )
+
+(defonce incomming-actions-state
+  (atom {}))
 
 
 
@@ -49,11 +84,11 @@
 (defn incomming-actions
   "Parse incomming actions to Bomberman rule engine facts"
   [incomming-state game-state]
-  (->> @incomming-state
-       vals
-       (map vals)
-       (apply concat)
-       (map (partial command->engine-fact game-state))))
+  (some->> @incomming-state
+           vals
+           (map vals)
+           (apply concat)
+           (map (partial command->engine-fact game-state))))
 
 (defn game-state->enginge-facts [game-state]
   (let [gs      @game-state
@@ -74,78 +109,97 @@
      fire
      bombs)))
 
+(defn remove-stones-from-gs [gs stones-to-remove]
+  (let [stones-to-remove' (->> stones-to-remove (map :position-xy) (set))
+        stones            (:stones gs)]
+    (assoc gs :stones (remove stones-to-remove' stones))))
 
-(defn game-loop [task-execution-timestamp game-state incomming-state ws-broadcast-fn]
-  (def ws-broadcast-fn ws-broadcast-fn)
-  (def game-state game-state)
-  (def incomming-state incomming-state)
+(defn update-bombs-to-gs [gs bombs]
+  (assoc gs :bombs (set bombs)))
+
+(defn update-fire-to-gs [gs fires]
+  (assoc gs :fire (set fires)))
+
+(defn update-user-position-in-gs [gs {:keys [user-id next-position] :as user-move}]
+  (assoc-in gs [:players user-id :position] next-position))
+
+(defn update-users-position-in-gs [gs user-moves]
+  (reduce update-user-position-in-gs gs user-moves))
+
+(defn update-user-is-dead [gs dead-users]
+  (reduce
+   (fn [gs' {:keys [user-id killed-by-user-id] :as dead-user}]
+     (-> gs'
+         (assoc-in [:players user-id :dead?] true)
+         (assoc-in [:dead-users user-id] dead-user)))
+   gs
+   dead-users))
+
+(defn apply-actions-to-game-state [gs {:keys [actions]}]
+  (let [{:keys [bombs-on-board fire-on-board stones-to-remove user-moves dead-users]} actions]
+    (-> @gs
+        (remove-stones-from-gs       stones-to-remove)
+        (update-bombs-to-gs          bombs-on-board)
+        (update-fire-to-gs           fire-on-board)
+        (update-users-position-in-gs user-moves)
+        (update-user-is-dead         dead-users))))
+
+(defn game-loop [task-execution-timestamp game-state incomming-actions-state ws-broadcast-fn]
   (println "Game loop is now started " task-execution-timestamp)
-  (let [user-action-facts (incomming-actions incomming-state game-state)
-        game-state-facts  (game-state->enginge-facts game-state)]
-    (def user-action-facts user-action-facts)
-    (def game-state-facts game-state-facts)
-    (bomberman-rules/run-rules
-     (concat
-      user-action-facts
-      game-state-facts
-      [(bomberman-rules/->TimestampNow (java.util.Date.))]))
-    )
+  (let [user-action-facts   (incomming-actions incomming-actions-state game-state)
+        _                   (def user-action-facts user-action-facts)
+        game-state-facts    (game-state->enginge-facts game-state)
+        _                   (def game-state-facts game-state-facts)
+        actions-from-enging (bomberman-rules/run-rules
+                             (concat
+                              user-action-facts
+                              game-state-facts
+                              [(bomberman-rules/->TimestampNow (java.util.Date.))]))
+        _                   (def actions-from-enging actions-from-enging)
+        new-game-state      (apply-actions-to-game-state game-state actions-from-enging)
+        _                   (def new-game-state new-game-state)]
+    (reset! game-state new-game-state)
+    (reset! incomming-actions-state {})
 
+    #_(ws-broadcast-fn [:new/game-state new-game-state]))
 
   ;; (ws-broadcast-fn [:test/broadcast "hejsan"])
   (println "Game loop is now done " (java.util.Date.))
-  )
+  new-game-state)
 
 
 (defmulti register-incomming-user-action! :action)
 
 (defmethod register-incomming-user-action! :move [{:keys [action user-id] :as m}]
-  (swap! incomming-state assoc-in [action user-id] m))
+  (swap! incomming-actions-state assoc-in [action user-id] m))
 
 (defmethod register-incomming-user-action! :place-bomb [{:keys [action user-id] :as m}]
-  (swap! incomming-state assoc-in [action user-id] m))
+  (swap! incomming-actions-state assoc-in [action user-id] m))
 
 (defmethod register-incomming-user-action! :default [m]
   (throw (Exception. (str "In dont know what to do with" m))))
 
 
+(comment
+  (game-loop (java.util.Date.) game-state incomming-actions-state (fn []))
+  @game-state
 
+  (register-incomming-user-action!
+   {:action  :move
+    :user-id 1
+    :payload {:direction :east}})
 
-
-(register-incomming-user-action!
- {:action  :move
-  :user-id 1
-  :payload {:direction :west}})
-
-(register-incomming-user-action!
- {:action  :place-bomb
-  :user-id 1})
-
-
-;; @game-state
-;; (reset! incomming-state {})
-
-
-
+  (register-incomming-user-action!
+   {:action  :place-bomb
+    :user-id 1})
+  )
 
 
 (defn system [{:keys [scheduler timbre webserver ws-handler http-handler]}]
   (timbre/info "Creating system.")
   (component/system-map
-   :game-state        (atom {:players {1 {:position    [1 1]
-                                          :fire-length 3
-                                          :id          1
-                                          :sign        "1"}}
-                             :stones  [[4 1] [3 3]]
-                             :board   (board/init 6)
-                             :bombs   [{:user-id          1
-                                        :bomb-position-xy [3 2]
-                                        :fire-length      3
-                                        :bomb-added-timestamp (java.util.Date.)}]
-                             :fire    [#_{:user-id              6
-                                        :fire-position-xy     [3 2]
-                                        :fire-start-timestamp (java.util.Date.)}]})
-   :incomming-actions (atom {})
+   :game-state        game-state
+   :incomming-actions incomming-actions-state
    :logging           (components.timbre/create timbre)
    :scheduler         (component/using
                        (components.chime/create scheduler)
@@ -161,7 +215,7 @@
                        [:logging :router])))
 
 
-(def production
+(defonce production
   (system
    {:http-handler #'server.endpoints/handler
     :webserver    {:port 3005}
