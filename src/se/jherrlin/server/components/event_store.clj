f7e5567f5ea19a3ac81cc59938b99ca0a817ea8f
John Herrlin <john.herrlin@fortnox.se>
2021-09-10 15:10:12 +0200

    event-store and game-state as components

---
+++ src/se/jherrlin/server/components/event_store.clj
@@ -0,0 +1,64 @@
+(ns se.jherrlin.server.components.event-store
+  (:require [com.stuartsierra.component :as component]
+            [taoensso.timbre :as timbre]))
+
+(defonce store-init {:events '()})
+(defonce store (atom store-init))
+
+(defn add-event [store event]
+  (swap! store update :events conj event))
+
+(defrecord EventStore [args]
+  component/Lifecycle
+
+  (start [this]
+    (if (:server this)
+      (do
+        (timbre/info "Starting EventStore component but server is already runnig.")
+        this)
+      (do
+        (timbre/info "Starting EventStore component.")
+        (assoc this
+               :store     store
+               :add-event (partial add-event store)))))
+
+  (stop [this]
+    (if (get this :store)
+      (do
+        (timbre/info "Stopping EventStore component.")
+        (reset! store store-init)
+        (assoc this :store nil :add-event nil))
+      (do
+        (timbre/info "Stopping EventStore component but no server instance found!")
+        this))))
+
+(defn create
+  "Create a new EventStore component."
+  []
+  (map->EventStore {:args nil}))
+
+(comment
+  (add-watch store :watcher
+             (fn [key atom old-state new-state]
+               (prn "-- Atom Changed --")
+               (prn "key" key)
+               (prn "atom" atom)
+               (prn "atom type" (type atom))
+               (prn "old-state" old-state)
+               (prn "new-state" new-state)
+               (prn "latest" (-> new-state :events first))))
+
+  @store
+  (reset! store store-init)
+
+  (add-event {:a :b})
+  (add-event {:c :d})
+  (add-event {:abc :def})
+
+  ;; https://en.wikipedia.org/wiki/Conflict-free_replicated_data_type
+  ;; Används av Google Docs
+
+  ;; https://en.wikipedia.org/wiki/Raft_(algorithm)
+
+  ;; https://orbitdb.org/
+  )
---
+++ src/se/jherrlin/server/components/game_state.clj
@@ -0,0 +1,43 @@
+(ns se.jherrlin.server.components.game-state
+  (:require [com.stuartsierra.component :as component]
+            [taoensso.timbre :as timbre]))
+
+
+(def initial-game-state {})
+(defonce game-state (atom initial-game-state))
+
+(defrecord GameState [args event-store]
+  component/Lifecycle
+
+  (start [this]
+    (if (:game-state this)
+      (do
+        (timbre/info "Starting GameState component but server is already runnig.")
+        this)
+      (let [projection-fn (:projection-fn args)]
+        (timbre/info "Starting GameState component.")
+        (add-watch (:store event-store) :game-state-projection
+                   (fn [key atom old-state new-state]
+                     (when-let [latest-event (-> new-state :events first)]
+                       (let [old-game-state @game-state
+                             new-game-state (projection-fn old-game-state latest-event)]
+                         (reset! game-state new-game-state)))))
+        (assoc this
+               :projection-fn projection-fn
+               :game-state    game-state))))
+
+  (stop [this]
+    (if (get this :game-state)
+      (do
+        (timbre/info "Stopping GameState component.")
+        (reset! game-state initial-game-state)
+        (remove-watch (:store event-store) :game-state-projection)
+        (assoc this :projection-fn nil :game-state nil))
+      (do
+        (timbre/info "Stopping GameState component but no instance found!")
+        this))))
+
+(defn create
+  "Create a new GameState component."
+  [projection-fn]
+  (map->GameState {:args {:projection-fn projection-fn}}))
