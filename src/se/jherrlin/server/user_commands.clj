d6c30648260ba99620c6e672dd7bbe1afc50818a
John Herrlin <john.herrlin@fortnox.se>
2021-09-11 08:16:45 +0200

    user-commands updated with game-id

--- src/se/jherrlin/server/user_commands.clj
+++ src/se/jherrlin/server/user_commands.clj
@@ -1,50 +1,51 @@
 (ns se.jherrlin.server.user-commands
   (:require [clojure.spec.alpha :as s]))
 
-(defmulti register-incomming-user-action! (fn [incomming-actions-state action-payload] (:action action-payload)))
+(defmulti register-incomming-user-action! (fn [incomming-actions-state command] (:action command)))
 
-(defmethod register-incomming-user-action! :move [incomming-actions-state {:keys [action user-id] :as m}]
-  (swap! incomming-actions-state assoc-in [action user-id] m))
+(defmethod register-incomming-user-action! :move [incomming-actions-state {:keys [game-id action user-id] :as m}]
+  (swap! incomming-actions-state assoc-in [game-id action user-id] m))
 
-(defmethod register-incomming-user-action! :place-bomb [incomming-actions-state {:keys [action user-id] :as m}]
-  (swap! incomming-actions-state assoc-in [action user-id] m))
+(defmethod register-incomming-user-action! :place-bomb [incomming-actions-state {:keys [game-id action user-id] :as m}]
+  (swap! incomming-actions-state assoc-in [game-id action user-id] m))
 
-(defmethod register-incomming-user-action! :throw-bomb [incomming-actions-state {:keys [action user-id] :as m}]
-  (swap! incomming-actions-state assoc-in [action user-id] m))
+(defmethod register-incomming-user-action! :throw-bomb [incomming-actions-state {:keys [game-id action user-id] :as m}]
+  (swap! incomming-actions-state assoc-in [game-id action user-id] m))
 
 (defmethod register-incomming-user-action! :default [incomming-actions-state m]
   (throw (Exception. (str "In dont know what to do with" m))))
 
 
-
+(s/def ::game-id    (s/or :s string? :u uuid?))
 (s/def ::action     #{:place-bomb :move :throw-bomb})
 (s/def ::user-id    number?)
 (s/def ::direction  #{:west :east :north :south})
-(s/def ::payload    (s/keys :req-un [::direction]))
-(s/def ::move       (s/keys :req-un [::action ::user-id ::payload]))
-(s/def ::place-bomb (s/keys :req-un [::action ::user-id]))
+(s/def ::move       (s/keys :req-un [::game-id ::action ::user-id ::direction]))
+(s/def ::place-bomb (s/keys :req-un [::game-id ::action ::user-id]))
 (s/def ::actions
   (s/or :move       ::move
         :place-bomb ::place-bomb))
 
 (comment
   (s/valid? ::move
-            {:action  :move
-             :user-id 1
-             :payload {:direction :east}})
+            {:game-id   (java.util.UUID/randomUUID)
+             :user-id   1
+             :action    :move
+             :direction :east})
+
   (s/valid? ::move
-            {:action  :move
-             :user-id 1
-             :payload {:direction :east}})
-  (s/valid? ::actions
-            {:action  :move
-             :user-id 1})
-  (s/valid? ::move
-            {:action  :move1
-             :user-id 1
-             :payload {:direction :east}})
+            {:game-id   (java.util.UUID/randomUUID)
+             :action    :move1
+             :user-id   1
+             :direction :east})
 
   (s/valid? ::actions
-            {:action  :throw-bomb
+            {:game-id "ett UUID"
+             :action  :throw-bomb
+             :user-id 1})
+
+  (s/valid? ::actions
+            {:game-id (java.util.UUID/randomUUID)
+             :action  :place-bomb
              :user-id 1})
   )
