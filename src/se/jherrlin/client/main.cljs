601f33a75554195a9a2c7df05ea4bf9f4f8b8b55
John Herrlin <john.herrlin@fortnox.se>
2021-09-16 07:54:42 +0200

    basic client router

--- src/se/jherrlin/client/main.cljs
+++ src/se/jherrlin/client/main.cljs
@@ -2,6 +2,8 @@
   (:require [re-frame.core :as re-frame]
             [reagent.dom :as rd]
             [reitit.core :as r]
+            [reitit.frontend.easy :as rfe]
+            [se.jherrlin.client.router :as client.router]
             [se.jherrlin.client.events :as events]
             [reitit.frontend.easy :as rfe]
             ["semantic-ui-react" :as semantic-ui]))
@@ -11,8 +13,7 @@
   (let []
     [:div {:style {:flex "1"}}
      [:> semantic-ui/Menu {:size       "large"
-                           :borderless true
-                           :style      {:background "#80ba27"}}
+                           :borderless true}
       [:> semantic-ui/Menu.Item
        {:as "a" :href (rfe/href :route1/home)} "Home"]
       [:> semantic-ui/Menu.Item
@@ -20,77 +21,40 @@
 
       [:> semantic-ui/Menu.Menu {:position "right" }
        [:> semantic-ui/Menu.Item
-        #_[:> semantic-ui/Image {:as     "a"
-                               :href "asd"
-                               :src    picture
-                               :avatar true}]]
+        [:img {:src "img/bomberman-logo.png" :style {:height "2em"}}]]]]]))
 
-       [:> semantic-ui/Menu.Item
-        [:img {:src "img/logo.png" :style {:width "5em"}}]]]]]))
+(defn main-component [{:keys [router]}]
+  (let [current-route @(re-frame/subscribe [:current-route])]
+    (def router router)
+    [:> semantic-ui/Container
+     {:style {:height         "100%"
+              :display        "flex"
+              :flex-direction "column"}}
 
-(defn main-component []
-  [:> semantic-ui/Container
-   {:style {:height         "100%"
-            :display        "flex"
-            :flex-direction "column"}}
+     ;; Header
+     [header-menu router]
 
-   ;; Header
-   [:div {:style {:flex "1"}}
-     [:> semantic-ui/Menu {:size       "large"
-                           :borderless true
-                           }
-      [:> semantic-ui/Menu.Item
-       {:as "a"
-        ;:href (rfe/href :route1/home)
-        } "Home"]
-      [:> semantic-ui/Menu.Item
-       {:as "a"
-        ;:href (rfe/href :route2/db)
-        } "Info"]
+     ;; Submenu
+     [:div {:style {:flex "1" :padding-top "0.5em" :flex-direction "column"}}]
 
-      [:> semantic-ui/Menu.Item
-       {:as "a"
-        ;:href (rfe/href :route2/db)
-        } "Create new game"]
+     ;; Main body
+     [:div {:style {:height      "100%"
+                    :overflow-y  "auto"
+                    :overflow-x  "hidden"
+                    :padding-top "0.5em"}}
+      (when current-route
+        (-> current-route :data :view))]
 
-      [:> semantic-ui/Menu.Item
-       {:as "a"
-        ;:href (rfe/href :route2/db)
-        } "Join a game"]
-
-
-      [:> semantic-ui/Menu.Menu {:position "right" }
-       [:> semantic-ui/Menu.Item
-        #_[:> semantic-ui/Image {:as     "a"
-                               :href "asd"
-                               :src    picture
-                               :avatar true}]]
-
-       [:> semantic-ui/Menu.Item
-        [:img {:src "img/bomberman-logo.png" :style {:height "2em"}}]]]]]
-
-   [:div {:style {:flex           "1"
-                  :padding-top    "0.5em"
-                  :flex-direction "column"}}
-    ;; Submenu
-    ]
-
-   [:div {:style {:height      "100%"
-                  :overflow-y  "auto"
-                  :overflow-x  "hidden"
-                  :padding-top "0.5em"}}
-    ;; This is the main location on the page.
-    ]
-
-   [:div
-    [:> semantic-ui/Menu
-     [:> semantic-ui/Menu.Item {:name "Notifikationer"}]
-     [:> semantic-ui/Menu.Item {:name "Något"}]]]])
+     #_[:div
+      [:> semantic-ui/Menu
+       [:> semantic-ui/Menu.Item {:name "Notifikationer"}]
+       [:> semantic-ui/Menu.Item {:name "Något"}]]]]))
 
 (defn ^:dev/after-load mount-root []
   (re-frame/clear-subscription-cache!)
-  (rd/render [main-component] (.getElementById js/document "app")))
+  (rd/render [main-component {:router (client.router/handler)}] (.getElementById js/document "app")))
 
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
+  (client.router/init)
   (mount-root))
--- src/se/jherrlin/client/router.cljs
+++ src/se/jherrlin/client/router.cljs
@@ -1,1 +1,90 @@
-(ns se.jherrlin.client.router)
+(ns se.jherrlin.client.router
+  (:require [re-frame.core :as re-frame]
+            [reitit.coercion.spec :as rss]
+            [reitit.core :as r]
+            [reitit.frontend :as rf]
+            [reitit.frontend.controllers :as rfc]
+            [reitit.frontend.easy :as rfe]))
+
+
+(re-frame/reg-event-db
+ ::initialize-routes
+ (fn [db _]
+   (merge {:current-route nil} db)))
+
+
+(re-frame/reg-event-fx
+ :push-state
+ (fn [_ [_ & route]]
+   {:push-state route}))
+
+(re-frame/reg-event-db
+ ::navigated
+ (fn [db [_ new-match]]
+   (let [old-match   (:current-route db)
+         controllers (rfc/apply-controllers (:controllers old-match) new-match)]
+     (assoc db :current-route (assoc new-match :controllers controllers)))))
+
+(re-frame/reg-sub
+ :current-route
+ (fn [db]
+   (:current-route db)))
+
+(re-frame/reg-fx :push-state
+  (fn [route]
+    (apply rfe/push-state route)))
+
+(defn on-navigate [new-match]
+  (when new-match
+    (re-frame/dispatch [::navigated new-match])))
+
+(def routes
+  [["/"
+    {:name      :route1/home
+     :link-text "Home"
+     :view      [:div "Home"]
+     :controllers
+     [{;; Do whatever initialization needed for home page
+       ;; I.e (re-frame/dispatch [::events/load-something-with-ajax])
+       :start (fn [& params]
+                (js/console.log "Entering home page")
+                #_(re-frame/dispatch [::push-state ::home]))
+       ;; Teardown can be done here.
+       :stop  (fn [& params] (js/console.log "Leaving home page"))}]}]
+   ["/db"
+    {:name      :route2/db
+     :link-text "db"
+     :view      [:div "db"]
+     :controllers
+     [{:start (fn [& params]
+                (js/console.log "Entering db page")
+                #_(re-frame/dispatch [::push-state ::db]))
+       :stop  (fn [& params] (js/console.log "Leaving db page"))}]}]])
+
+(defn handler []
+  (rf/router
+   [routes
+    ;; routes
+    ]
+   {:data {:coercion rss/coercion}}))
+
+(defn init-routes! []
+  (js/console.log "initializing routes")
+  (rfe/start!
+   (handler)
+   on-navigate
+   {:use-fragment true}))
+
+(def debug? ^boolean goog.DEBUG)
+
+(defn dev-setup []
+  (when debug?
+    (enable-console-print!)
+    (println "dev mode")))
+
+(defn init []
+  (re-frame/clear-subscription-cache!)
+  (re-frame/dispatch-sync [::initialize-routes])
+  (dev-setup)
+  (init-routes!) ;; Reset routes on figwheel reload
+)
