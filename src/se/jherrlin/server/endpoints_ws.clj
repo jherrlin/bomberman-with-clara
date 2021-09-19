cbef4f1d47f2ff0012fa6b10cfa982c8f88c496a
John Herrlin <john.herrlin@fortnox.se>
2021-09-19 08:35:57 +0200

    cleanup in endpoints ws

--- src/se/jherrlin/server/endpoints_ws.clj
+++ src/se/jherrlin/server/endpoints_ws.clj
@@ -16,10 +16,8 @@
   [{:keys [?reply-fn] :as req}]
   (?reply-fn {:server-sending-in-ws "JA-JA-JA"}))
 
-
 (defmethod handler :command/user-action
   [{:keys [?reply-fn incomming-actions ?data] :as req}]
-  (def req req)
   (user-commands/register-incomming-user-command! incomming-actions ?data))
 
 (defmethod handler :game/create
@@ -27,51 +25,35 @@
   (def req req)
   (def game-state (-> req :game-state))
   (def add-events-fn! (-> req :event-store :add-events-fn!))
-  (let [{:keys [?data ?reply-fn client-id event-store incomming-actions projection-fn game-state send-fn]} req
-        {:keys [add-events-fn!]}                                                                           event-store]
+  (let [{:keys [?data ?reply-fn event-store game-state]} req
+        {:keys [add-events-fn!]}                         event-store]
     (?reply-fn
-     (application-service/create-game! game-state add-events-fn! (assoc ?data :action :create-game))
-     )
-
-    )
-  )
-
-{:action        :create-game
- :game-name     ""
- :game-password ""}
-
+     (application-service/create-game! game-state add-events-fn! (assoc ?data :action :create-game)))))
 
 (defmethod handler :game/list
   [req]
-  (let [{:keys [?data ?reply-fn client-id event-store game-state incomming-actions]} req
-        {:keys [game-name password]}                                                 ?data]
+  (let [{:keys [?reply-fn]} req]
     (?reply-fn (->> req :game-state deref :games vals
                     (filter (comp #{:created} :game-state))
                     (map #(select-keys % [:game-name :subject]))))))
 
 (defmethod handler :game/join
   [req]
-  (let [{:keys [?data ?reply-fn client-id event-store incomming-actions projection-fn game-state send-fn]} req
-        {:keys [add-events-fn!]}                                                                           event-store]
+  (let [{:keys [?data ?reply-fn event-store game-state]} req
+        {:keys [add-events-fn!]}                         event-store]
     (?reply-fn
      (application-service/join-game! game-state add-events-fn! (assoc ?data :action :join-game)))))
 
 (defmethod handler :game/start
   [req]
-  (def req req)
-  (let [{:keys [?data ?reply-fn client-id event-store incomming-actions projection-fn game-state send-fn]} req
-        {:keys [add-events-fn!]}                                                                           event-store]
-    (let [v (application-service/start-game! game-state add-events-fn! (assoc ?data :action :start-game))]
-      (def v v)
-      (?reply-fn v))))
-
+  (let [{:keys [?data ?reply-fn event-store game-state]} req
+        {:keys [add-events-fn!]}                         event-store]
+    (?reply-fn (application-service/start-game! game-state add-events-fn! (assoc ?data :action :start-game)))))
 
 (defmethod handler :game/events
   [req]
-  (def req req)
-  (let [{:keys [?data ?reply-fn client-id event-store incomming-actions projection-fn game-state send-fn]} req
-        {:keys [game-id]}                                                                                  ?data
-        {:keys [add-events-fn!]}                                                                           event-store]
+  (let [{:keys [?data ?reply-fn event-store]} req
+        {:keys [game-id]}                     ?data]
     (?reply-fn
      (->> event-store
           :store
