fed87ff84b642c7e0c4ac3cc9f700d4a78f553df
John Herrlin <john.herrlin@fortnox.se>
2021-09-21 07:49:14 +0200

    test on started game projection and facts

---
+++ test/se/jherrlin/clara_labs/started_game_state_facts_test.clj
@@ -0,0 +1,49 @@
+(ns se.jherrlin.clara-labs.started-game-state-facts-test
+  (:require [clojure.test :as t]
+            [se.jherrlin.server.resources :as resources]
+            [se.jherrlin.server.models :as models]
+            [clojure.pprint :as pprint]
+            [se.jherrlin.server.game-state2 :as game-state2]
+            [se.jherrlin.server.game-state :as game-state]
+            [se.jherrlin.clara-labs.board :as board]))
+
+
+(comment
+  (->> (let [game-id 1]
+         [(models/->CreateGame #inst "2021-09-20T20:15:34.000-00:00" game-id "GameName" "pwd" board/mini '() '())
+          (models/->JoinGame   #inst "2021-09-20T20:15:40.000-00:00" game-id 1 "Player 1")
+          (models/->JoinGame   #inst "2021-09-20T20:15:42.000-00:00" game-id 2 "Player 2")
+          (models/->JoinGame   #inst "2021-09-20T20:15:44.000-00:00" game-id 3 "Player 3")
+          (models/->StartGame  #inst "2021-09-20T20:15:47.000-00:00" game-id)])
+       (map #(.toCloudEvent %))
+       (game-state/the-projection {})
+       :games
+       (vals)
+       (first)
+       (game-state2/facts)
+       (resources/output "/home/john/git/clara-labs-frontend/resources/test-data/started-game-facts.edn"))
+  )
+
+
+
+(t/deftest game-state-facts
+  (t/testing "Make sure that events projections create the same facts."
+    (t/is
+     (=
+      (->> (let [game-id 1]
+             [(models/->CreateGame #inst "2021-09-20T20:15:34.000-00:00" game-id "GameName" "pwd" board/mini '() '())
+              (models/->JoinGame   #inst "2021-09-20T20:15:40.000-00:00" game-id 1 "Player 1")
+              (models/->JoinGame   #inst "2021-09-20T20:15:42.000-00:00" game-id 2 "Player 2")
+              (models/->JoinGame   #inst "2021-09-20T20:15:44.000-00:00" game-id 3 "Player 3")
+              (models/->StartGame  #inst "2021-09-20T20:15:47.000-00:00" game-id)])
+           (map #(.toCloudEvent %))
+           (game-state/the-projection {})
+           :games
+           (vals)
+           (first)
+           (game-state2/facts)
+           (map #(into {} %))
+           (set))
+      (->> (resources/pull "test-data/started-game-facts.edn")
+           (map #(into {} %))
+           (set))))))
---
+++ test/se/jherrlin/clara_labs/started_game_state_projections_test.clj
@@ -0,0 +1,36 @@
+(ns se.jherrlin.clara-labs.started-game-state-projections-test
+  (:require  [clojure.test :as t]
+             [se.jherrlin.server.resources :as resources]
+             [se.jherrlin.server.models :as models]
+             [clojure.pprint :as pprint]
+             [se.jherrlin.server.game-state :as game-state]
+             [se.jherrlin.clara-labs.board :as board]))
+
+(comment
+  (->> (let [game-id 1]
+         [(models/->CreateGame #inst "2021-09-20T20:15:34.000-00:00" game-id "GameName" "pwd" board/mini '() '())
+          (models/->JoinGame   #inst "2021-09-20T20:15:40.000-00:00" game-id 1 "Player 1")
+          (models/->JoinGame   #inst "2021-09-20T20:15:42.000-00:00" game-id 2 "Player 2")
+          (models/->JoinGame   #inst "2021-09-20T20:15:44.000-00:00" game-id 3 "Player 3")
+          (models/->StartGame  #inst "2021-09-20T20:15:47.000-00:00" game-id)])
+       (map #(.toCloudEvent %))
+       (game-state/the-projection {})
+       (pprint/pprint)
+       (with-out-str)
+       (spit "/home/john/git/clara-labs-frontend/resources/test-data/started-game-state.edn"))
+  )
+
+
+(t/deftest started-game-state-projection
+  (t/testing "Make sure the game state projection stays the same"
+    (t/is
+     (=
+      (resources/read-edn-file "test-data/started-game-state.edn")
+      (->> (let [game-id 1]
+             [(models/->CreateGame #inst "2021-09-20T20:15:34.000-00:00" game-id "GameName" "pwd" board/mini '() '())
+              (models/->JoinGame   #inst "2021-09-20T20:15:40.000-00:00" game-id 1 "Player 1")
+              (models/->JoinGame   #inst "2021-09-20T20:15:42.000-00:00" game-id 2 "Player 2")
+              (models/->JoinGame   #inst "2021-09-20T20:15:44.000-00:00" game-id 3 "Player 3")
+              (models/->StartGame  #inst "2021-09-20T20:15:47.000-00:00" game-id)])
+           (map #(.toCloudEvent %))
+           (game-state/the-projection {}))))))
